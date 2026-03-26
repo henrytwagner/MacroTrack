f20ea877ac9af43e65aaa83aabb1fda966a9588f
henrytwagner <htwags22@gmail.com>
2026-03-26 11:55:43 -0400

    fix: add openssl to Dockerfile, restore datasource url in    prisma.config.ts

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -3,4 +3,7 @@ import path from "node:path";
 
 export default defineConfig({
   schema: path.join(__dirname, "src/db/prisma/schema.prisma"),
+  datasource: {
+    url: process.env.DATABASE_URL ?? "",
+  },
 });
